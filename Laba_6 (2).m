5137c8a21f8d7a150b142d86ce57d57cabedf623
sofya-on <47686226+sofya-on@users.noreply.github.com>
2020-05-16 06:52:22 +0300

    Update Laba_6 (2).m

--- Laba_6 (2).m
+++ Laba_6 (2).m
@@ -1,53 +1,29 @@
-## Copyright (C) 2020 nikit
-## 
-## This program is free software: you can redistribute it and/or modify it
-## under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
-## 
-## This program is distributed in the hope that it will be useful, but
-## WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-## 
-## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see
-## <https://www.gnu.org/licenses/>.
 
-## -*- texinfo -*- 
-## @deftypefn {} {@var{retval} =} Laba_6 (@var{input1}, @var{input2})
-##
-## @seealso{}
-## @end deftypefn
-
-## Author: nikit <nikit@LAPTOP-F2VC9HFF>
-## Created: 2020-05-08
-
-function retval = Laba_6 (values, interval, step)
-  tochnostb = 3;
-  iterations = min(length(values), tochnostb);
-  start = interval(1);
-  stop = interval(2);
-  x = start:step:stop;
-  table(:, 1) = values(:);
-  for i = 2:iterations
+function retval = netderivative (values, span, step)
+  accuracy = 3;
+  iter = min(length(values), accuracy);
+  first = span(1);
+  last = span(2);
+  x = first:step:last;
+  a(:, 1) = values(:);
+  for i = 2:iter
     for j = 1:length(values) - i+1
-      table(j, i) = -table(j, i-1) + table(j+1, i-1);
+      a(j, i) = -a(j, i-1) + a(j+1, i-1);
     end
   end
   
-  for i = 1:length(values)-iterations
-    rez = 0;
+  for i = 1:length(values)-iter
+    res = 0;
     sign = 1;
-    for j = 2:iterations
-      rez += (table(i, j)*sign) / (j-1);
+    for j = 2:iter
+      res += (a(i, j)*sign) / (j-1);
       sign = sign*-1;
       
     end
-    proizv(i) = rez/step;
+    der(i) = res/step;
   end
-  retval = proizv;
-  plot(x, values,"r", x(1:length(x)-iterations), proizv, "g");
+  retval = der;
+  plot(x, values,"r", x(1:length(x)-iter), der, "g");
   
       
 endfunction
